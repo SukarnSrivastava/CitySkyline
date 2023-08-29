a1acc2fb5f62c0c579f549ba04dfe11c02bf28df
Sukarn Srivastava <sukarnsrivastava@gmail.com>
2023-08-29 07:40:00 +0530

    initial commit

---
+++ Personal_Portfolio/script.js
@@ -0,0 +1,27 @@
+var typed = new Typed('#element', {
+    strings: ['Web Developer', 'Front-end developer', 'UI/UX designer', 'React.js developer', 'Web Designer'],
+    typeSpeed: 70,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
+
+const circles = document.querySelectorAll('.circle');
+circles.forEach(elem=>{
+    var dots = elem.getAttribute("data-dots");
+    var marked = elem.getAttribute("data-percent");
+    var percent = Math.floor((dots*marked)/100);
+    var points = "";
+    var rotate = 360 / dots;
+
+    for (let i=0; i < dots; i++){
+        points +=`<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+    }
+    elem.innerHTML = points;
+
+    const pointsMarked = elem.querySelectorAll('.points');
+    for(let i=0; i<percent; i++){
+        pointsMarked[i].classList.add('marked')
+    }
+    
+})
